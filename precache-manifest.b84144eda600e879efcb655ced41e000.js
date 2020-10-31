9b7e396629979c1b255402d3a2a5ff51eb69e883
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-31 17:46:16 +0530

    Updates

---
+++ precache-manifest.b84144eda600e879efcb655ced41e000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a1ae8350407ad1611b4db7a2e19941",
+    "url": "/donjude-eng/monsters-App/index.html"
+  },
+  {
+    "revision": "1fd217bae2b61c40d0e9",
+    "url": "/donjude-eng/monsters-App/static/css/main.e75202ad.chunk.css"
+  },
+  {
+    "revision": "671eca9f7fb9c378bfbb",
+    "url": "/donjude-eng/monsters-App/static/js/2.c7505f2e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/donjude-eng/monsters-App/static/js/2.c7505f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd217bae2b61c40d0e9",
+    "url": "/donjude-eng/monsters-App/static/js/main.73f1a13f.chunk.js"
+  },
+  {
+    "revision": "a52d6bf22ca3ffad9b54",
+    "url": "/donjude-eng/monsters-App/static/js/runtime-main.63be5860.js"
+  }
+]);
